496b7684f168180c23163caf45a2af5c9b81d062
Maheep Gupta <91566211+CodingStuffsofMahee@users.noreply.github.com>
2023-06-30 02:05:24 +0530

    project completed for current forecast

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import WeatherCard from './components/WeatherCard';
 import HourWeather from './components/HourWeather';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 // import weatherData from './data';
 // import weatherData from './data';
+
 
 
 function App() {
   const [weatherData, setweatherData] = useState(null)
-  const [location, setLocation] = useState('India')
-  useEffect(() => {
-    try {
-      fetch(
-        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&days=1&aqi=no&alerts=no`)
-        .then(response => response.json()).then((weatherJsonData) =>
-          setweatherData(weatherJsonData)
-          // setweatherData(null)
+  const [location, setLocation] = useState('')
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    location===''?setweatherData(null):
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&days=1&aqi=no&alerts=no`)
+      .then(response => response.json()).then((weatherJsonData) =>
+        setweatherData(weatherJsonData)
+        // setweatherData(null)
 
-        )
-    } catch (error) {
-      setweatherData(null)
-    }
+      )
+      .catch(error =>
+        setweatherData(null)
+      )
 
-  }, [])
-  console.log(weatherData)
-  console.log(process.env.API_KEY)
+
+  }
+
+
+
 
 
 
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
+      <div className='customSearch p-2'>
+
+        <form className="d-flex " onSubmit={handleSubmit}>
+          <input className="form-control me-2" type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Search" aria-label="Search" />
+          <input className="btn btn-outline-success" type='submit' />
+        </form>
+      </div>
+
+
       {
         weatherData !== null ?
           <div className="App">
-            {console.log(weatherData)}
             <WeatherCard location={weatherData['location']} forecastCurrent={weatherData['current']} />
             <div className='box-hour-weather'>
               {weatherData['forecast'].forecastday[0].hour.map((hour, index) => {
@@ -45,7 +57,8 @@ function App() {
 
               })}
             </div>
-          </div> : <div>No lOcation Selected</div>}
+          </div> : <div style={{ color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>No location Selected</div>}
+
     </>
   )
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,17 @@
-// import React from 'react'
+import React from 'react'
+import logo from './logo.png'
 
-// const Navbar = () => {
-//     return (
-//         <nav className="navbar" style={{backgroundColor:"#e3f2fd"}}>
-//         <div className="container-fluid">
-//           <a className="navbar-brand" href="#">Navbar</a>
-//           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <a className="nav-link active" aria-current="page" href="#">Home</a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#">Link</a>
-//               </li>
-//               <li className="nav-item dropdown">
-//                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//                   Dropdown
-//                 </a>
-//                 <ul className="dropdown-menu">
-//                   <li><a className="dropdown-item" href="#">Action</a></li>
-//                   <li><a className="dropdown-item" href="#">Another action</a></li>
-//                   <li><hr className="dropdown-divider"></li>
-//                   <li><a className="dropdown-item" href="#">Something else here</a></li>
-//                 </ul>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link disabled">Disabled</a>
-//               </li>
-//             </ul>
-//             <form className="d-flex" role="search">
-//               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-//               <button className="btn btn-outline-success" type="submit">Search</button>
-//             </form>
-//           </div>
-//         </div>
-//       </nav>
-//     )
-// }
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-body-dark" >
+            <div className="container-fluid">
+                <a className="navbar-brand " style={{color:'#fff'}} href="/">
+                    <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                        Weather Me
+                </a>
+            </div>
+        </nav>
+    )
+}
 
-// export default Navbar
+export default Navbar
